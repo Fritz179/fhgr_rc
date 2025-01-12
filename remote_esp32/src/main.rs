e5cb57ec14766736da55d91252acbefa6e29591d
Fritz 179 <fabrizio.cortesi179@gmail.com>
2025-01-12 18:20:05 +0100

    Remote start

---
+++ remote_esp32/src/main.rs
@@ -0,0 +1,33 @@
+#![no_std]
+#![no_main]
+
+use esp_backtrace as _;
+use esp_hal::{
+    delay::Delay,
+    gpio::{Io, Level, Output},
+    prelude::*,
+};
+use esp_println::println;
+
+#[entry]
+fn main() -> ! {
+    let peripherals = esp_hal::init(esp_hal::Config::default());
+    println!("Hello, world!");
+
+    // Set GPIO7 as an output, and set its state high initially.
+    let mut led = Output::new(peripherals.GPIO20, Level::Low);
+
+    led.set_high();
+
+    // Initialize the Delay peripheral, and use it to toggle the LED state in a
+    // loop.
+    let delay = Delay::new();
+    let mut i = 0;
+
+    loop {
+        led.toggle();
+        println!("Hello, world! {i}");
+        i += 1;
+        delay.delay_millis(500u32);
+    }
+}
